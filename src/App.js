5ae5c441abfd7c16ee9e706d630f9e1c387d9874
Royal-lobster <srujangs8@gmail.com>
2020-07-25 07:28:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import CurrencyRow from "./components/CurrencyRow";
 
-function App() {
+export default function App() {
+  const URI ='https://api.exchangeratesapi.io/latest'
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  useEffect(()=>{
+    fetch(URI)
+      .then(res=> res.json())
+      .then(data=>{
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      })
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+  console.log(currencyOptions)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen bg-gray-700">
+      <div className="m-auto bg-red-400 p-6 rounded">
+        <center>
+          <h1 className="text-3xl text-white">Currency Converter</h1>
+          <hr className="m-3 border-dashed border-gray-300"></hr>
+          <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={fromCurrency}/>
+          <h1 className="text-2xl"> = </h1>
+          <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={toCurrency}/>
+        </center>
+      </div>
     </div>
   );
 }
-
-export default App;
