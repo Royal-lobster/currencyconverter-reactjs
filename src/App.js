ef316b81046d5c64bd67f2efb675e65e7fa9be0d
Royal-lobster <srujangs8@gmail.com>
2020-07-25 07:46:12 +0530

    commented code

--- src/App.js
+++ src/App.js
@@ -2,27 +2,40 @@ import React, { useEffect, useState } from "react";
 import CurrencyRow from "./components/CurrencyRow";
 
 export default function App() {
-  const URI ='https://api.exchangeratesapi.io/latest'
+  //api url
+  const URI = "https://api.exchangeratesapi.io/latest";
+  //states
   const [currencyOptions, setCurrencyOptions] = useState([]);
   const [fromCurrency, setFromCurrency] = useState();
   const [toCurrency, setToCurrency] = useState();
-  useEffect(()=>{
+  //use effect to fetch all the data form api and store in states before.
+  useEffect(() => {
     fetch(URI)
-      .then(res=> res.json())
-      .then(data=>{
-        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
-      })
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
-  console.log(currencyOptions)
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+      });
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  //return
   return (
     <div className="flex h-screen bg-gray-700">
       <div className="m-auto bg-red-400 p-6 rounded">
         <center>
           <h1 className="text-3xl text-white">Currency Converter</h1>
           <hr className="m-3 border-dashed border-gray-300"></hr>
-          <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={fromCurrency}/>
+          {/* ----------from currency row----------- */}
+          <CurrencyRow
+            currencyOptions={currencyOptions}
+            selectedCurrency={fromCurrency}
+          />
+          {/* --------end of from currency row------- */}
           <h1 className="text-2xl"> = </h1>
-          <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={toCurrency}/>
+          {/* -------------to currency row------------ */}
+          <CurrencyRow
+            currencyOptions={currencyOptions}
+            selectedCurrency={toCurrency}
+          />
+          {/* --------end of to currency row------- */}
         </center>
       </div>
     </div>
--- src/components/CurrencyRow.js
+++ src/components/CurrencyRow.js
@@ -1,15 +1,18 @@
 import React from "react";
 
-function CurrencyRow({currencyOptions}) {
+function CurrencyRow({ currencyOptions }) {
   return (
     <>
-      <input type="number" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline mb-2"></input>
+      <input
+        type="number"
+        className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline mb-2"
+      ></input>
       {/* select field styling */}
       <div className="inline-block relative w-64">
-        {/* ------------select field------------ */}
+        {/* ------000---select field-----000---- */}
         <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-        {currencyOptions.map((currency)=>{
-            return <option key={currency}>{currency}</option>
+          {currencyOptions.map((currency) => {
+            return <option key={currency}>{currency}</option>;
           })}
         </select>
         {/* ---------end of select field-------- */}
